dc1bebf05a10abe3267087fc881eda4a20cd564a
ankita-singh27 <rajputanki5@gmail.com>
2024-05-25 13:55:55 +0530

    create post completed

--- routes/blog.js
+++ routes/blog.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 
 const {createComment} = require("../controllers/CommentController");
-
+const {createPost, getAllPosts} = require("../controllers/PostController");
 
 
 //Mapping Create
 
 router.post("/comments/create", createComment);
+router.post("/posts/create", createPost);
+router.get("/posts", getAllPosts);
 
 //export
 module.exports = router;
